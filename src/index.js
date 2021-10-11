31404e7fc930ce58109480c442a5cfce2c10443d
cuilei <allen5205189@gmail.com>
2021-10-11 09:00:07 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import "./styles.css";
+import "./styles.scss";
 import "font-awesome/css/font-awesome.min.css";
 
 const toast = document.querySelector(".toast");
+const sin = document.getElementById('in');
+const sout = document.getElementById('out');
 
 function slidein() {
-  toast.classList = "slidein";
+  toast.classList.remove("slideout")
+  toast.classList.add("slidein")
 }
 
 function slideout() {
-  toast.classList = "slideout";
+  toast.classList.add("slideout")
+  toast.classList.remove("slidein")
 }
+
+function remove() {
+  slideout()
+  setTimeout(() => toast.remove(), 1000)
+}
+
+toast.addEventListener('animationend', remove)
+sin.addEventListener('click', slidein)
+sout.addEventListener('click', slideout)
